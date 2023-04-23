65386d369aaf8c0d6b2975b7853bfb938960d2e3
warmec <warmec@oregonstate.edu>
2023-04-23 01:15:55 +0000

    Completed Lab 2

--- StandardLibrary/standard_library.py
+++ StandardLibrary/standard_library.py
@@ -1,8 +1,8 @@
 # standard_library.py
 """Python Essentials: The Standard Library.
-<Name>
-<Class>
-<Date>
+Camden Warme
+MTH 420
+4/21/2023
 """
 
 
@@ -11,6 +11,10 @@ def prob1(L):
     """Return the minimum, maximum, and average of the entries of L
     (in that order, separated by a comma).
     """
+    return [min(L), max(L), sum(L)/len(L)]
+try:
+    print(prob1([1, 2, 3, 4, 5]))
+except:
     raise NotImplementedError("Problem 1 Incomplete")
 
 
@@ -19,14 +23,50 @@ def prob2():
     """Determine which Python objects are mutable and which are immutable.
     Test integers, strings, lists, tuples, and sets. Print your results.
     """
+    #testing integers
+    five = int(5)
+    five2=five
+    five=five+1
+    if five==five2:
+        print("Integers are mutable")
+    else:
+        print("Integers are immutable")
+    #testing strings
+    word = "blake"
+    word2=word
+    word += "rules"
+    if word==word2:
+        print("Strings are mutable")
+    else:
+        print("Strings are immutable")
+    #testing lists
+    blah=['x', 'y']
+    blahh=blah
+    blahh.append('z')
+    if blah==blahh:
+        print("Lists are mutable")
+    else:
+        print("Lists are immutable")
+    #testing tuples
+    print("Since no syntax allows tuples to be modified, they must be immutable")
+    #testing sets
+    myset={1111, 11, 111}
+    myset2=myset
+    myset.add(111111111111)
+    if myset==myset2:
+        print("Sets are mutable")
+    else:
+        print("Sets are immutable")
+try:
+    prob2()
+except:
     raise NotImplementedError("Problem 2 Incomplete")
 
 
 # Problem 3
-def hypot(a, b):
-    """Calculate and return the length of the hypotenuse of a right triangle.
+"""Calculate and return the length of the hypotenuse of a right triangle.
     Do not use any functions other than sum(), product() and sqrt() that are
-    imported from your 'calculator' module.
+    imported from your 'calculator' .
 
     Parameters:
         a: the length one of the sides of the triangle.
@@ -34,6 +74,13 @@ def hypot(a, b):
     Returns:
         The length of the triangle's hypotenuse.
     """
+def hypot(a, b):
+    import numpy as np
+    import calculator as c
+    return np.sqrt(c.sum(c.product(a, a), c.product(b, b)))
+try:
+    print(hypot(3, 4))
+except:
     raise NotImplementedError("Problem 3 Incomplete")
 
 
@@ -47,6 +94,16 @@ def power_set(A):
     Returns:
         (list(sets)): The power set of A as a list of sets.
     """
+    import itertools
+    power_set=[]
+    for i in range(len(A)+1):
+        for j in list(itertools.combinations(A, i)):
+            power_set.append(list(j))
+    return [set(subset) for subset in power_set]
+try:
+    print(power_set('hi'))
+    
+except:
     raise NotImplementedError("Problem 4 Incomplete")
 
 
